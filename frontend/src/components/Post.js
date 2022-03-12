d04146c97a6dfc3f13ef50ffd78730215366144b
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2022-03-12 17:25:51 +0100

    Add dislike, like and comment count

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,6 +1,21 @@
-import { connect, useDispatch, useSelector} from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react"
 function Post(props) {
-    let post = useSelector(state => state.posts.list.find(post=>post.id==props.postid));
+    let post = useSelector(state => state.posts.list.find(post => post.id == props.postid));
+    let like_activated = "/images/like-active.svg"
+    let like_deactivated = "/images/like.svg";
+    let like_image = like_deactivated;
+    let dislike_image = like_deactivated;
+    switch (post.like_state) {
+        case "LIKE":
+            dislike_image = like_deactivated;
+            like_image = like_activated;
+            break;
+        case "DISLIKE":
+            dislike_image = like_activated;
+            like_image = like_deactivated;
+    }
+
     return (<div className="w-full bg-primary rounded-xl mt-3 p-3 shadow-md">
         <div className="float-left rounded-full h-12 mr-2 w-12 bg-center bg-no-repeat bg-contain shadow-sm" style={{ backgroundImage: `url("${post.user.image}")` }}></div>
         <div className="float-left text-sm text-accent-3">{post.user.name}</div>
@@ -10,10 +25,12 @@ function Post(props) {
         </div>
         <br></br>
         <div className="flex">
-            <div style={{ "backgroundImage": "url('/images/comment.svg')" }} className="h-12 w-12 bg-center bg-auto bg-no-repeat m-3 shadow-md"></div>
-            <div className="text-accent-1">{post.likes}</div>
-            <div style={{ "backgroundImage": "url('/images/like.svg')" }} className="h-12 w-12 bg-center bg-cover bg-no-repeat m-3 shadow-md"></div>
-            <div style={{ "backgroundImage": "url('/images/like.svg')" }} className="h-12 w-12 bg-top-left bg-cover bg-no-repeat rotate-180 m-3 shadow-md"></div>
+            <div className="" style={{ "backgroundImage": "url('/images/comment.svg')" }} className="h-12 w-12 bg-center bg-auto bg-no-repeat m-3"></div>
+            <div className="mt-auto mb-auto text-accent-1">{post.comments}</div>
+            <div style={{ "backgroundImage": `url('${like_image}')` }} className="h-12 w-12 bg-center bg-cover bg-no-repeat m-3"></div>
+            <div className="mt-auto mb-auto text-accent-1">{post.likes}</div>
+            <div style={{ "backgroundImage": `url('${dislike_image}')` }} className="h-12 w-12 bg-top-left bg-cover bg-no-repeat rotate-180 m-3"></div>
+            <div className="mt-auto mb-auto text-accent-1">{post.dislikes}</div>
         </div>
     </div>)
 }
