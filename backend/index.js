5d65b83015a81eefbaa405f723c3092e3d40bca3
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2022-03-22 10:56:06 +0100

    Add backend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use("/", [function (req, res, next) {
+    serveLogin(req, res);
+}, express.static(__dirname + "/../frontend/build/")]);
+
+function serveLogin(req, res) {
+    let file = req.path;
+    switch (file) {
+        case "/":
+        case "/index.html":
+            res.sendFile(path.resolve(__dirname + "/../login/index.html"));
+            break;
+        case "/index.css":
+            res.sendFile(path.resolve(__dirname + "/../login/index.css"));
+            break;
+        case "/index.js":
+            res.sendFile(path.resolve(__dirname + "/../login/index.js"));
+            break;
+        case "/signup":
+            res.sendFile(path.resolve(__dirname + "/../login/signup/index.html"));
+            break;
+        case "/signup/index.css":
+            res.sendFile(path.resolve(__dirname + "/../login/signup/index.html"));
+            break;
+        case "/signup/index.js":
+            res.sendFile(path.resolve(__dirname + "/../login/signup/index.js"));
+            break;
+        default:
+            res.redirect("/");
+            break;
+    }
+}
+app.listen(8080)
