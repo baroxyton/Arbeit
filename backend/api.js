d68c9a6cba7431fac1ea01583b3186d0e1d52ff7
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2022-03-25 19:06:06 +0100

    Basic verification on submitted data

--- backend/api.js
+++ backend/api.js
@@ -1,4 +1,5 @@
 const usernameRegex = /^[a-zA-Z]([a-zA-Z\-_0-9]){1,20}$/;
+const charRegex = /[a-zA-Z\-_0-9]/;
 function api(req, res) {
     function sendJSON(json) {
         res.send(JSON.stringify(json));
@@ -10,11 +11,33 @@ function api(req, res) {
     switch (param1) {
         case "signup":
             console.log(req.body)
-            const { username, passwort } = req.body;
-            if (!username || !passwort) {
+            const { username, password } = req.body;
+            if (!username || !password) {
                 sendJSON({
                     status: "error",
                     error: "Kein Nutzername oder Passwort!"
+                });
+                return;
+            }
+            if (username.length > 21) {
+                sendJSON({
+                    status: "error",
+                    error: "Nutzername zu lang! Die länge sollte zwischen 2 und 21 liegen."
+                });
+                return;
+            }
+            if (username.length < 2) {
+                sendJSON({
+                    status: "error",
+                    error: "Nutzername zu kurz! Die länge sollte zwischen 2 und 21 liegen."
+                });
+                return;
+            }
+            if (!username.match(usernameRegex)) {
+                const disallowedChar = username.split("").find(char => !char.match(charRegex));
+                sendJSON({
+                    status: "error",
+                    error: `Nutzername enthält unerlaubtes Zeichen: "${disallowedChar || "Am anfang"}". Bitte verwenden Sie nur Buchstaben, Zahlen, _ und -. Das erste Zeichen muss ein Buchstabe sein`
                 })
             }
             break;
