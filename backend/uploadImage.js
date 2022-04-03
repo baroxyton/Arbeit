d1780c79126689090c4a99a34aff46927cddd662
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2022-04-03 20:51:19 +0200

    Add uploadImage function

---
+++ backend/uploadImage.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const apiKey = fs.readFileSync(__dirname + "/.imgbb-key").toString();
+const ibb_upload = require("imgbb-uploader");
+async function uploadImage(base64, name) {
+    const output = await ibb_upload({
+        apiKey,
+        base64string:base64,
+        name
+    });
+    console.log(await output, "output");
+    
+}
+module.exports = uploadImage;
